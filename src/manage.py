559f4799e42373ab27bc6cd6cb756e9ebe98fc71
Brian Cunningham <bcunningham@niyamit.com>
2021-08-23 14:09:36 -0500

    fix(manage): fix tool update logic

--- src/manage.py
+++ src/manage.py
@@ -83,20 +83,22 @@ class Manage:
         os.environ["HTTPS_PROXY"] = self.proxy
 
 
-    def condaInstallHazPy(self):
+    def create_conda_environment(self):
         """ Uses conda to install the latest version of hazpy
         """
         print('Checking for the conda environment {ve}'.format(ve=self.virtual_environment))
         try:
-            try:
-                check_call('{ca} {ve}'.format(ca=self.conda_activate, ve=self.virtual_environment), shell=True)
-            except:
-                try:
-                    print('Creating the conda virtual environment {ve}'.format(ve=self.virtual_environment))
-                    self.handleProxy()
-                    call('echo y | conda env create --file {ey}'.format(ey=self.env_yaml), shell=True)
-                except:
-                    call('{cd} && conda env remove -n {ve}'.format(cd=self.conda_deactivate, ve=self.virtual_environment), shell=True)
+        #     try:
+        #         # Check if environment exists (return == 0)
+        #         check_call('{ca} {ve}'.format(ca=self.conda_activate, ve=self.virtual_environment), shell=True)
+        #     except:
+        #         try:
+            print('Creating the conda virtual environment {ve}'.format(ve=self.virtual_environment))
+            self.handleProxy()
+            call('echo y | conda env create --file {ey}'.format(ey=self.env_yaml), shell=True)
+        except:
+            # TODO: Remove this? - Will be replaced with self.update_environment() - BC
+            call('{cd} && conda env remove -n {ve}'.format(cd=self.conda_deactivate, ve=self.virtual_environment), shell=True)
 
             #print('Installing {pp}'.format(pp=self.python_package))
             #self.handleProxy()
@@ -107,23 +109,24 @@ class Manage:
                 # check_call('{ca} {ve} && echo y | conda env update -n {ve} --file {ey}'.format(ca=self.conda_activate, ve=self.virtual_environment, ey=self.env_yaml), shell=True)
 
             self.messageBox(0, u'The Hazus Export Tool was successfully installed! The update will take effect when the tool is reopened.', u"HazPy", 0x1000 | 0x4)
-        except:
-            self.messageBox(0, u'Unable to install the Hazus Export Tool If this error persists, contact hazus-support@riskmapcds.com for assistance.', u"HazPy", 0x1000 | 0x4)
+# TODO: Remove this - BC
+        # except:
+        #     self.messageBox(0, u'Unable to install the Hazus Export Tool If this error persists, contact hazus-support@riskmapcds.com for assistance.', u"HazPy", 0x1000 | 0x4)
 
-
-    def createHazPyEnvironment(self):
-        returnValue = self.messageBox(None, u'The newest Export Tool is required to run this tool. Would you like to install it now?', u"HazPy", 0x1000 | 0x4)
-        try:
-            if returnValue == 6:
-                ctypes.windll.user32.ShowWindow(
-                    ctypes.windll.kernel32.GetConsoleWindow(), 1)
-                #print("Installing {pp} - hold your horses, this could take a few minutes... but it's totally worth it".format(pp=self.python_package))
-                #print('Conda is installing {pp}'.format(pp=self.python_package))
-                print('Conda is installing the Export Tool')
-                self.condaInstallHazPy()
-        except Exception as e:
-            print(e)
-            self.messageBox(0, u"An error occured. The Export Tool was not installed. Please check your network settings and try again.", u"HazPy", 0x1000 | 0x4)
+# TODO: Remove this? - BC
+    # def createHazPyEnvironment(self):
+    #     returnValue = self.messageBox(None, u'The newest Export Tool is required to run this tool. Would you like to install it now?', u"HazPy", 0x1000 | 0x4)
+    #     try:
+    #         if returnValue == 6:
+    #             ctypes.windll.user32.ShowWindow(
+    #                 ctypes.windll.kernel32.GetConsoleWindow(), 1)
+    #             #print("Installing {pp} - hold your horses, this could take a few minutes... but it's totally worth it".format(pp=self.python_package))
+    #             #print('Conda is installing {pp}'.format(pp=self.python_package))
+    #             #print('Conda is installing the Export Tool')
+    #             self.condaInstallHazPy()
+    #     except Exception as e:
+    #         print(e)
+    #         self.messageBox(0, u"An error occured. The Export Tool was not installed. Please check your network settings and try again.", u"HazPy", 0x1000 | 0x4)
 
     def checkForUpdates(self):
         print('Checking for tool updates')
@@ -146,18 +149,24 @@ class Manage:
                     returnValue = self.messageBox(
                         None, u"A new version of the tool was found. Would you like to install it now?", u"HazPy", 0x1000 | 0x4)
                     if returnValue == 6:
-                        print('updating tool')
+                        print('Updating tool...')
                         self.updateTool()
-                        if self.isCondaInPath():
-                            self.condaInstallHazPy()
-                #self.createHazPyEnvironment
+                        # Check if conda is in path
+                if self.isCondaInPath():
+                    # Check if environment exists
+                    res = run('{ca} {ve}'.format(ca=self.conda_activate, ve=self.virtual_environment), shell=True, capture_output=True)
+                    # Update environment if it exists (res.returncode == 0)
+                    if res.returncode == 0:
+                        self.update_environment()
+                    # Create environmnent if it does not exists (res.returncode == 1)
+                    else:
+                        self.create_conda_environment()
             else:
                 print('Unable to connect to url: ' + self.tool_version_url)
         except:
             self.messageBox(0, 'Unable to check for tool updates. If this error persists, contact hazus-support@riskmapcds.com for assistance.', "HazPy", 0x1000 | 0x4)
 
     def updateTool(self):
-
         try:
             from distutils.dir_util import copy_tree
             from io import BytesIO
@@ -173,8 +182,8 @@ class Manage:
             toDirectory = './'
             copy_tree(fromDirectory, toDirectory)
             rmtree(fromDirectory)
-            self.messageBox(
-                0, u'The tool was successfully updated! I hope that was quick enough for you. The update will take effect when the tool is reopened.', u"HazPy", 0x1000 | 0x4)
+            # self.messageBox(
+            #     0, u'The tool was successfully updated! I hope that was quick enough for you. The update will take effect when the tool is reopened.', u"HazPy", 0x1000 | 0x4)
         except:
             self.messageBox(
                 0, u'The tool update failed. If this error persists, contact hazus-support@riskmapcds.com for assistance.', u"HazPy", 0x1000 | 0x4)
@@ -187,7 +196,6 @@ class Manage:
             version = version.replace(i, '')
         version = version.strip()
         return version
-
 
     def internetConnected(self):
         cnxn = self.handleProxy()
@@ -226,7 +234,6 @@ class Manage:
         os.environ['HTTP_PROXY'] = ''
         os.environ['HTTPS_PROXY'] = ''
 
-    #def startApp(self, app_path, update_path):
     def startApp(self, app_path):
         """[summary]
 
@@ -234,24 +241,28 @@ class Manage:
             app_path ([type]): [description]
             update_path ([type]): [description]
         """
-        print('Opening the app and checking for updates')
+        print('Starting the HAZUS Export application...')
+        # TODO: Move this - BC
         if self.isCondaInPath():
-            if self.conda_activate:
                 try:
-                    #self.checkForUpdates()
-                    res = run('{ca} {ve}'.format(ca=self.conda_activate, ve=self.virtual_environment), shell=True, capture_output=True)
-                    if 'Could not find' in str(res):
-                        # create the virtual environment if it does not exists
-                        self.createHazPyEnvironment()
-                    else:
-                        #call('{ca} {ve} && start /min python {up}'.format(ca=self.conda_activate, ve=self.virtual_environment, up=update_path), shell=True)
-                        #call('{ca} {ve} && start python {ap}'.format(ca=self.conda_activate, ve=self.virtual_environment, ap=app_path), shell=True)
-                        run('{ca} {ve} && start python {ap}'.format(ca=self.conda_activate, ve=self.virtual_environment, ap=app_path), shell=True)
-                        #call('start python {ap}'.format(ap=app_path), shell=True)
-                except:
+                    run('{ca} {ve} && python {ap}'.format(ca=self.conda_activate, ve=self.virtual_environment, ap=app_path), shell=True)
+                    #call('start python {ap}'.format(ap=app_path), shell=True)
+                except Exception as e:
+                    print(e)
                     error = str(sys.exc_info()[0])
                     self.messageBox(0, u"Unexpected error: {er} | If this problem persists, contact hazus-support@riskmapcds.com.".format(er=error), u"HazPy", 0x1000 | 0x4)
-            else:
-                self.messageBox(0, u"Error: Anaconda was found in your system PATH variable, but was unable to activate. Please check to make sure your system PATH variable is pointing to the correct Anaconda root, bin, and scripts directories and try again.\nIf this problem persists, contact hazus-support@riskmapcds.com.", u"HazPy", 0x1000 | 0x4)
         else:
             self.messageBox(0, u"Error: Unable to find conda in the system PATH variable. Add conda to your PATH and try again.\n If this problem persists, contact hazus-support@riskmapcds.com.", u"HazPy", 0x1000 | 0x4)
+
+
+# TODO: Update Environment if version has changed - BC
+# source: https://medium.com/@balance1150/how-to-build-a-conda-environment-through-a-yaml-file-db185acf5d22
+    def update_environment(self):
+        try:
+            #conda deactivate export_env
+            res = run('conda deactivate', shell=True, capture_output=True)
+            if res.returncode == 0:
+                #conda env update -f <path_to_yaml_file>
+                call('echo y | conda env update --file {ey}'.format(ey=self.env_yaml), shell=True)
+        except Exception as e:
+            print(e)
